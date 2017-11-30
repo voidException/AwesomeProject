f1c8392bec0d7fc8d01ffefb8fa2a5e78f6919a3
apple <apple@appledeMac-mini.local>
2017-11-30 15:18:31 +0800

    学习react-navigation 和redux集成

--- App.js
+++ App.js
@@ -1,86 +1,23 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View, Button
-} from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-console.log(Ionicons)
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import AppReducer from './src/reducers';
+import AppWithNavigationState from './src/navigators/AppNavigator';
 
-const HomeScreen = ({navigation}) => (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-            onPress={() => navigation.navigate('Details')}
-            title="Go to details"
-        />
-    </View>
-);
+export  default  class App extends React.Component {
+    store = createStore(AppReducer); //把reducer注入到store中
 
-const DetailsScreen = () => (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-    </View>
-);
+    render() {
+        return (
+            <Provider store={this.store}>
+                <AppWithNavigationState />
+            </Provider>
+        );
+    }
+}
 
-const App = StackNavigator({
-    Home: {
-        screen: HomeScreen,
-        navigationOptions: {
-            headerTitle: 'Home',
-            tabBarIcon: ({ tintColor, focused }) => (
-                <Ionicons
-                    name={focused ? 'ios-home' : 'ios-home-outline'}
-                    size={26}
-                    style={{ color: tintColor }}
-                />
-            ),
-        },
-    },
-    Details: {
-        screen: DetailsScreen,
-        navigationOptions: {
-            headerTitle: 'Details',
-        },
-    },
-});
-
-export default App;
-
-
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ src/navigators/AppNavigator.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addNavigationHelpers, StackNavigator } from 'react-navigation';
+
+import LoginScreen from '../components/LoginScreen';
+import MainScreen from '../components/MainScreen';
+import ProfileScreen from '../components/ProfileScreen';
+
+//定义路由的顶端入口
+export const AppNavigator = StackNavigator({
+  Login: { screen: LoginScreen },
+  Main: { screen: MainScreen },
+  Profile: { screen: ProfileScreen },
+});
+
+//终极入口，加入了redux支持
+const AppWithNavigationState = ({ dispatch, nav }) => (
+  <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
+);
+
+AppWithNavigationState.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  nav: PropTypes.object.isRequired,
+};
+
+//从store中取出state传递给该页面
+const mapStateToProps = state => ({
+  nav: state.nav,
+});
+
+export default connect(mapStateToProps)(AppWithNavigationState);
